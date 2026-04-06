5d0c52aed6f3b06443a2c8ca0419564ee4f2d576
Nikomus <nick@nikomus.games>
2026-04-06 13:24:02 +0100

    Add initial facilitator content

--- src/routes/facilitators/+page.svelte
+++ src/routes/facilitators/+page.svelte
@@ -1,5 +1,4 @@
 <script>
-	import Session from '../../components/Session.svelte';
 </script>
 
 <main>
@@ -22,10 +21,18 @@
 					</p>
 
 					<p>
-						Alongside their paid work Ray also runs a housing co-operative, provides facilitation
-						and culture development support to Radical Routes (a network of co-ops) and regularly
-						facilitates community organising groups such as OUT prison abolition group, and the
-						Queer Care Collective.
+						Alongside their paid work Ray also runs a <a
+							href="https://raythespirit.com/blog/2025/8/28/moving-at-the-speed-of-love"
+							>housing co-operative</a
+						>, provides facilitation and culture development support to
+						<a href="https://raythespirit.com/blog/2026/2/16/love-amp-solidarity-at-radical-routes"
+							>Radical Routes</a
+						>
+						(a network of co-ops) and regularly facilitates community organising groups such as
+						<a href="https://out-hull.webnode.co.uk/">OUT prison abolition group</a>, and the
+						<a href="https://www.canva.com/design/DAG5huWWv9c/u32yY-jYB7ilpVJLF2f-pQ/edit"
+							>Queer Care Collective</a
+						>.
 					</p>
 
 					<p>
@@ -34,10 +41,100 @@
 					</p>
 
 					<p>
-						They also regularly write about the nuances of facilitating different dynamics and
-						groups.
+						They also <a href="https://raythespirit.com/holding-space-together">regularly write</a>
+						about the nuances of facilitating different dynamics and groups.
 					</p>
 				</div>
+			</li>
+
+			<li>
+				<div>
+					<h3>Abi (she/her)</h3>
+
+					<p>
+						<a href="https://www.linkedin.com/in/abimurphy/">Abi Handley</a> is a facilitator and team
+						coach, supporting teams to embed effective working practices and centering equity, awareness
+						about power and effective collaborative approaches to working.
+					</p>
+
+					<p>
+						Her focus in work is to support people within teams to be conscious of how they are
+						working with and for each-other and those they are centering their work around, adapting
+						and iterating on their practices to change the dynamics that are central in our system
+						to be more equitable.
+					</p>
+
+					<p>
+						Her specific experience in equity, power and money work is wide and varied, from
+						developing radical approaches to pay within her own co-operative organisation to
+						supporting funding organisations and networks to distribute funding collaboratively, and
+						working with numerous organisations around their own approach to power, hierarchy,
+						equity and value exchange.
+					</p>
+
+					<a href="https://www.linkedin.com/in/abimurphy/">LinkedIn</a>
+					<a href="https://alpackacollective.com/">Website</a>
+				</div>
+			</li>
+
+			<li>
+				<h3>Benedicta (she/her)</h3>
+
+				<p>
+					Benedicta is a Creative Practice Director and founder of <a
+						href="https://www.goldensankofa.com/">Golden Sankofa</a
+					>, a social enterprise dedicated to transforming places and spaces that nurture community
+					wellbeing, belonging and ownership.
+				</p>
+				<p>
+					After a decade in local government, leading transformative projects from Camden's
+					Neighbourhood Spaces programme to post-Grenfell community assets portfolio, she
+					experienced burnout and anxiety that became a turning point. She left local government in
+					2022 and in 2024, she followed her dream of becoming a social entrepreneur, channeling her
+					expertise in co-design and place-based transformation into work centered on authentic
+					human connection. Her practice weaves strategic thinking with soul-nourishing experiences;
+					community dinners, retreats, and participatory workshops that honor diverse ways of being
+					and knowing.
+				</p>
+			</li>
+
+			<li>
+				<h3>Joel Chippindale (he/him)</h3>
+
+				<p>
+					Joel is an executive coach who supports talented technology leaders to accelerate their
+					development both 1-1 and in groups.
+				</p>
+				<p>
+					He is also an experienced facilitator and has spent nearly 20 years in leadership
+					positions, supporting people and teams to have better conversation and think more clearly
+					together.
+				</p>
+
+				<a href="https://monkeysthumb.co.uk">Website</a>
+			</li>
+
+			<li>
+				<h3>Elyem (she/her)</h3>
+
+				<p>
+					Elyem is an educator and organiser who supports groups to build their facilitation
+					practice in complex, real-world contexts. Her work is rooted in experiential learning and
+					political education, with a focus on supporting movements to increase their strategic and
+					political capacity.
+				</p>
+				<p>
+					She has designed and facilitated learning programmes and trainings with over 900 people
+					across the world, including in tenants’ unions, grassroots trade unions, and international
+					multicultural education spaces, working with diverse groups navigating difference, power,
+					and change. Her approach centres practice, supporting people to test ideas in real
+					situations, reflect on what happens, and refine their approach over time.
+				</p>
+				<p>
+					She is particularly interested in facilitation as a practice that moves people into action
+					and reflection, while building trust, autonomy, and shared responsibility in service of
+					collective liberation.
+				</p>
 			</li>
 		</ul>
 
@@ -83,10 +180,14 @@
 
 <style>
 	.hero {
+		background-image: url('$lib/assets/facilitators.webp');
+		background-position: center;
+		background-repeat: no-repeat;
+		background-size: contain;
 		align-items: center;
-		background-color: #eeeeee;
 		display: flex;
-		height: 10rem;
+		font-size: 3rem;
+		height: 14rem;
 		justify-content: center;
 		width: 100%;
 	}
@@ -97,8 +198,8 @@
 	}
 
 	li {
-		background-color: #eeeeee;
 		border-radius: 1rem;
+		border: 1px solid var(--orange);
 		list-style: none;
 		margin: 1rem 0;
 		min-width: 0;
